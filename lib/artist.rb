2d43d0913d6d2089781f20aed658f08a7bf4fc2d
Amyre Polk <myremyre101@icloud.com>
2020-07-26 00:28:28 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,18 +1,40 @@
-class Song
-  attr_accessor :name, :artist
+class Artist
+
+  attr_accessor :name, :songs
 
   @@all = []
 
   def initialize(name)
-    @name = name
-    @@all << self
-  end
+   @name = name
+   @songs = []
+   @@all << self
+ end
 
   def self.all
-    @@all
+   @@all  
+ end
+
+  def add_song(song)
+   @songs << song
+   song.artist = self
+ end
+
+  def songs
+ Song.all.select {|song| song.artist == self}
+ end
+
+  def self.find_or_create_by_name(artist_name)
+   artist = @@all.find{|a| a.name == artist_name}
+   return artist if artist
+   artist = Artist.new(artist_name)
+
+  #   # @@all.find{|a| a.name == artist_name} || Artist.new(artist_name)
+
   end
 
-  def artist_name
-    artist.name if artist
-  end
+  def print_songs
+   # @songs.each {|song| puts Song.artist.name}
+   # @@all.each {|song| puts song}
+  self.songs.each {|song| puts song.name}
+ end
 end
